95f7ce13a9eea8c4b47ef7b0613ed446afcc711c
Arshiya <arshiamehrabi50@gmail.com>
2024-08-12 02:56:29 +0330

    2

---
+++ main.py
@@ -0,0 +1,14 @@
+def phonepanel():
+    print("1- Add Contact")
+    print("2- Edit Contact")
+    print("3- Delete Contatct")
+    print("4- Contacts")
+    print("5- Sort Contacts")
+    print("6- Storage and Retrieval")
+    print("---------------------------------")
+    choice = input("Enter your choice: ")
+    if choice not in ["1","2","3","4","5","6"]:
+        print('not valid')
+        phonepanel()
+
+
