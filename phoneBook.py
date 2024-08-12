fed0cc97685993ecd0929fcb7f95fa97e0614f5a
Arshiya <arshiamehrabi50@gmail.com>
2024-08-12 13:43:57 +0330

    12

--- phoneBook.py
+++ phoneBook.py
@@ -20,6 +20,34 @@ def phonepanel():
         phonepanel()
     return choice
 
+
+def add_contact(self,contacts):
+    print(contact_name)
+    if len(contacts) != 0:
+        if self.name in contact_name: 
+            print('Duplicate entry')
+            print("---------------------------------")
+            return
+    
+    if not re.match(r'^[a-zA-Z\s]+$', self.name):
+        print('name is not correct')
+        print("---------------------------------")
+        return
+    if not re.match(r'^\d{11}$', self.phoneNumber):
+        print('number is not correct')
+        print("---------------------------------")
+        return
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', self.email):
+        print('email is not correct')
+        print("---------------------------------")
+        return
+    contacts.append({'Name': self.name, 'Number': self.phoneNumber, 'Email': self.email})
+    contact_name.append(self.name)
+    contact_numner.append(self.phoneNumber)
+    print('Contact added')
+    print("---------------------------------")
+
+
 phonepanel()
 while True:
     if choice == '1':
