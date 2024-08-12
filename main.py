5fccee29929fde65a3e9138f514aba435e34de91
Arshiya <arshiamehrabi50@gmail.com>
2024-08-12 11:17:10 +0330

    5

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from phoneBook import phone
+from phoneBook import Phone
 import csv
+
+
 
 
 file = open('contact.csv',newline='')
 data = csv.DictReader(file)
 contacts = list(data)
+
 
 def phonepanel():
     print("1- Add Contact")
@@ -12,8 +15,9 @@ def phonepanel():
     print("3- Delete Contatct")
     print("4- Contacts")
     print("5- Sort Contacts")
-    print("6- Storage and Retrieval")
+    print("6- Storage")
     print("---------------------------------")
+    global choice
     choice = input("Enter your choice: ")
     if choice not in ["1","2","3","4","5","6"]:
         print('not valid')
@@ -21,10 +25,20 @@ def phonepanel():
     return choice
 
 
+phonepanel()
+while True:
+    if choice == '1':
+        print('Enter the information')
+        name = input('Enter name: ')
+        phoneNumber = input('Enter phoneNumber: ')
+        email = input('Enter email: ')
+        phone = Phone(name,phoneNumber,email)
+        phone.add_contact(contacts)
+        phonepanel()
+
+    if choice == '2':
+            break
+
+
+
 print(contacts)
-
-
-
-
-
-
--- phoneBook.py
+++ phoneBook.py
@@ -1,13 +1,46 @@
 import re
 import csv
 
-class phone:
+class Phone:
 
     def __init__(self,name,phoneNumber,email):
         self.name = name
         self.phoneNumber = phoneNumber
         self.email = email
+
     
-    '''def check_exist_contact(self):'''
+    def add_contact(self,contacts):
+        if len(contacts) != 0:
+            for contact in contacts:
+                if contact['Number'] == self.phoneNumber or contact['Email'] == self.email:
+                    print('Duplicate entry')
+                    print("---------------------------------")
+                    return
+        
+        if not re.match(r'^[a-zA-Z\s]+$', self.name):
+            print('name is not correct')
+            print("---------------------------------")
+            return
+        if not re.match(r'^\d{11}$', self.phoneNumber):
+            print('number is not correct')
+            print("---------------------------------")
+            return
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', self.email):
+            print('email is not correct')
+            print("---------------------------------")
+            return
+        contacts.append({'Name': self.name, 'Number': self.phoneNumber, 'Email': self.email})
+        print('Contact added')
+        print("---------------------------------")
+
+    
+
+    
+
+
+
+
+       
+
 
 
